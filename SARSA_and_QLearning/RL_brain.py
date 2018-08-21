06b56db2bb079dc7f6bf16d40464b0778b6710dc
PoweredByME <saadkwi12@hotmail.com>
2018-08-21 13:58:21 +0500

    Added more structure to the code. Added alpha and epsilon decay params. Tested with an existing code and the results are equally optimal.

--- SARSA_and_QLearning/RL_brain.py
+++ SARSA_and_QLearning/RL_brain.py
@@ -1,14 +1,61 @@
 
-
-class RL_SARSA(object):
-    def __init__(self, agent, gamma = 0.9, alpha = 0.1, epsilon = 0.1, stepsPerEpisode = 50, noOfEpisodes = 2000):
+class RL_Brain(object):
+    '''
+        The parent class for SARSA and QLearning
+        algorithm classes.
+    '''
+    def __init__(self,
+                agent,
+                gamma = 0.9,
+                alpha = 0.1, 
+                epsilon = 0.1, 
+                alpha_decay = 0.005, 
+                epsilon_decay = 0.001, 
+                stepsPerEpisode = 50, 
+                noOfEpisodes = 2000
+                ):
         self.agent = agent;
         self.stepsPerEpisode = stepsPerEpisode;
         self.noOfEpisodes = noOfEpisodes;
         self.gamma = gamma;
+        self.alpha_decay = alpha_decay;
         self.alpha = alpha;
         self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay;
 
+    '''
+        This function performs all the episodes.
+        and it also decays the alpha. 
+    '''                
+    def Process(self, initState = None, showPolicyAfterEachBatch = False):
+        t = 1.0;
+        for _e in range(self.noOfEpisodes):
+            if _e % 100 == 0:
+                t += self.epsilon_decay;
+                self.epsilon = self.epsilon / t;
+            if _e % int(self.noOfEpisodes / 5) == 0:
+                print ("--> Episodes done:" + str(_e));
+                if showPolicyAfterEachBatch:
+                    print self.agent.environment.getPolicy();
+            self.takeStepsInEpisode(initState);
+
+    def takeStepsInEpisode(self, initState):
+        pass;
+#######################################################################
+
+class RL_SARSA(RL_Brain):
+    '''
+        This class implement the SARSA approach of the Temporal
+        Difference Learning.
+        Here the value function is extimated as:
+            Q(s,a) = Q(s,a) + alpha * (r + gamma * Q(s`,a`) - Q(s,a))
+        
+        This is on-policy algorithm.
+
+        This function performs an episode.
+        It ends if the required number of steps are completed or
+        a terminal state has been achieved.
+    '''
     def takeStepsInEpisode(self, initState):
         if initState is not None:
             self.agent.setState(initState[0], initState[1]);
@@ -32,25 +79,23 @@ class RL_SARSA(object):
                 break;                
             s = _s;
             a = _a;
-            self.agent.environment.updateCounts_sa[s][a] += 0.005;
+            self.agent.environment.updateCounts_sa[s][a] += self.alpha_decay;
             self.alpha = self.alpha / self.agent.environment.updateCounts_sa[s][a];
-                
-    def Process(self, initState = None):
-        for _e in range(self.noOfEpisodes):
-            self.takeStepsInEpisode(initState);    
+#######################################################################
 
+class RL_QLearning(RL_Brain):
+    '''
+        This class implement the SARSA approach of the Temporal
+        Difference Learning.
+        Here the value function is extimated as:
+            Q(s,a) = Q(s,a) + alpha * (r + gamma * argmax(Q(s`,a`)) - Q(s,a))
+    
+        This is off-policy algorithm.
 
-
-
-class RL_QLearning(object):
-    def __init__(self, agent, gamma = 0.9, alpha = 0.1, epsilon = 0.1, stepsPerEpisode = 50, noOfEpisodes = 2000):
-        self.agent = agent;
-        self.stepsPerEpisode = stepsPerEpisode;
-        self.noOfEpisodes = noOfEpisodes;
-        self.gamma = gamma;
-        self.alpha = alpha;
-        self.epsilon = epsilon
-
+        This function performs an episode.
+        It ends if the required number of steps are completed or
+        a terminal state has been achieved.
+    '''
     def takeStepsInEpisode(self, initState):
         if initState is not None:
             self.agent.setState(initState[0], initState[1]);
@@ -66,8 +111,7 @@ class RL_QLearning(object):
             if r is None: 
                 r = self.agent.environment.model.Value(loc[0],loc[1]); # if terminal
             (_a, loc, _r) = self.agent.getAction(self.epsilon);
-            (max_a, max_l, max_r) = self.agent.getAction(0.0);
-            SARSA_states = (s,a,r,_s,_a);
+            (max_a, max_l, max_r) = self.agent.getAction(-0.1);
             if _a is not None : # if not terminal
                 self.agent.environment.Q[s][a] += self.alpha * (r + self.gamma * self.agent.environment.Q[_s][max_a] - self.agent.environment.Q[s][a]);
                 self.agent.environment.statesVisited.append(s);
@@ -75,11 +119,8 @@ class RL_QLearning(object):
                 break;                
             s = _s;
             a = _a;
-            self.agent.environment.updateCounts_sa[s][a] += 0.005;
+            self.agent.environment.updateCounts_sa[s][a] += self.alpha_decay;
             self.alpha = self.alpha / self.agent.environment.updateCounts_sa[s][a];
                 
-    def Process(self, initState = None):
-        for _e in range(self.noOfEpisodes):
-            self.takeStepsInEpisode(initState);    
-
+    
 
--- SARSA_and_QLearning/main.py
+++ SARSA_and_QLearning/main.py
@@ -9,21 +9,24 @@ def main():
     problem(RL_brain.RL_QLearning);
 
 
-    
-
 def problem(learningClass):
     g = env.GridWorld(  
-                    5, 5,
-                    init_number = 0.015,
-                    obstacles = [ (1,2), (2,2), (3,2), (2,1), (3,1)],
-                    rewards = [(1,1,-40), (3,3,-50), (1,3,10)]
+                    4, 4,
+                    init_number = -0.10,
+                    obstacles = [(3,2),(1,2)],
+                    rewards = [(0,0,1),(0,3,10),(1,3,-1)]
                     );
     e = env.RL_Environment(g);
     a = agent.Agent(e);
 
-    brain = learningClass(a, noOfEpisodes = 100, epsilon = 0.1);
-    brain.Process((0,0));
-
+    brain = learningClass(  
+                        a, 
+                        noOfEpisodes = 200, 
+                        epsilon = 0.5, 
+                        epsilon_decay = 10.0/5000.0
+                        );
+    brain.Process(showPolicyAfterEachBatch = False);
+    print "Final Policy"
     print e.getPolicy();
 
 main();
